19ecd4f56b851e182632d7caa43901526af86594
tamziy <114886626+tamziy@users.noreply.github.com>
2023-11-16 19:23:50 -0600

    Made white-black Transitions

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-//222
+let isModalOpen = false;
+let contrastToggle = false;
+
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+
+
 
 function contact(event) {
     event.preventDefault();
@@ -23,8 +38,6 @@ function contact(event) {
         );
       })    
 }
-
-let isModalOpen = false;
 
 function toggleModal() {
     if (isModalOpen) {
