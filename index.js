e00af70a0c5313ca94652d14110bf23e23512559
tamziy <114886626+tamziy@users.noreply.github.com>
2023-11-23 14:49:47 -0600

    Add particle background effect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 50;
 
 
 function toggleContrast() {
@@ -11,9 +12,6 @@ function toggleContrast() {
     document.body.classList.remove("dark-theme");
   }
 }
-
-
-
 
 function contact(event) {
     event.preventDefault();
@@ -47,5 +45,17 @@ function toggleModal() {
 
     isModalOpen = true; 
     document.body.classList += " modal--open";
+}
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape")
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
 }
 
